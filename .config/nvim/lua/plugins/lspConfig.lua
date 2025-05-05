e5e31e9bf4ae827a611a92f7bc857de2ff43bf08
ashudevcodes <ashu.devcodes@gmail.com>
2025-05-05 21:06:30 +0530

    fix: now it will use mason download lsp

--- .config/nvim/lua/plugins/lspConfig.lua
+++ .config/nvim/lua/plugins/lspConfig.lua
@@ -1,45 +1,45 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-						disable = { "different-requires" },
-					},
-				},
-			},
-		})
-		lspconfig.gopls.setup({
-			settings = {
-				gopls = {
-					analyses = {
-						unusedparams = true,
-					},
-					staticcheck = true,
-					gofumpt = true,
-				},
-			},
-		})
-		lspconfig.clangd.setup({
-			name = "clangd",
-			cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
-			initialization_options = {
-				fallback_flags = { "-std=c++17" },
-			},
-		})
-		lspconfig.ts_ls.setup({
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"javascript.jsx",
-				"typescript",
-				"typescriptreact",
-				"typescript.tsx",
-			},
-			cmd = { "typescript-language-server", "--stdio" },
-		})
-	end,
+    "neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                        disable = { "different-requires" },
+                    },
+                },
+            },
+        })
+        lspconfig.gopls.setup({
+            settings = {
+                gopls = {
+                    analyses = {
+                        unusedparams = true,
+                    },
+                    staticcheck = true,
+                    gofumpt = true,
+                },
+            },
+        })
+        lspconfig.clangd.setup({
+            name = "clangd",
+            cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+            initialization_options = {
+                fallback_flags = { "-std=c++17" },
+            },
+        })
+        lspconfig.ts_ls.setup({
+            filetypes = {
+                "javascript",
+                "javascriptreact",
+                "javascript.jsx",
+                "typescript",
+                "typescriptreact",
+                "typescript.tsx",
+            },
+            cmd = { "typescript-language-server", "--stdio" },
+        })
+    end,
 }
